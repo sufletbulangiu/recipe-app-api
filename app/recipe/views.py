91dadad410cece1fc4467c27f733f4a793bc9dc3
Cosmin Manea <cosmin.manea@ibm.com>
2025-02-14 15:53:53 +0200

    Recipe Part Done

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,7 +12,7 @@ from recipe import serializer
 
 class ReceipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializer.RecipeSerializer
+    serializer_class = serializer.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -20,3 +20,15 @@ class ReceipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrive recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializer.RecipeSerializer
+        
+        return self.serializer_class
+    
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+    
